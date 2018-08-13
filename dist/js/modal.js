86f77610981dd9b1ea08f9027226c45c1db65fed
Mikhail Guskov <meguskov@gmail.com>
2018-08-13 08:13:41 +0300

    refactoring modal

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -1,75 +1,69 @@
-var Modal = (function() {
-  var trigger = $qsa('.modal__trigger');
-  //var modals = $qsa('.modal'); 
-  var modalsbg = $qsa('.modal__bg'); 
-  var content = $qsa('.modal__content'); 
-  var closers = $qsa('.modal__close'); 
-  var w = window;
-  var isOpen = false;
-  var contentDelay = 400; 
-  console.log(trigger);
+let Modal = (function() {
+  const trigger = $qsa('.modal__trigger');
+  const modalsbg = $qsa('.modal__bg');
+  const closers = $qsa('.modal__close');
+  let isOpen = false;
+  const contentDelay = 200;
 
-  var len = trigger.length;
+  const len = trigger.length;
 
   function $qsa(el) {
     return document.querySelectorAll(el);
   }
 
-  var getId = function(event) {
+  const handleClick = function(event) {
     event.preventDefault();
-
-    var self = this;
+    const modal = document.getElementById('modal');
+    const self = this;
     // получить значение атрибута data-modal от кнопки
     //var modalId = self.dataset.modal;
-    // выбираем модалку которую хотим активировать
-    var modal = document.getElementById('modal');
-    modal.className += ' active';
-    
+
+    modal.classList.add('active');
     makeDiv(self, modal);
   };
 
-  var makeDiv = function(self, modal) {
-    var fakediv = document.getElementById('modal__temp');
+  const makeDiv = function(self, modal) {
+    const fakediv = document.getElementById('modal__temp');
 
     if (fakediv === null) {
-      var div = document.createElement('div');
+      const div = document.createElement('div');
       div.id = 'modal__temp';
-      var coord = self.getBoundingClientRect();
+      const coord = self.getBoundingClientRect();
 
       div.style.top = coord.top + 'px';
       div.style.left = coord.left + 'px';
 
-      modalContent = document.getElementById('modal');
+      modal.appendChild(div);
 
-      modalContent.appendChild(div);
-      moveTrig(modalContent, modal, div);
+      window.requestAnimationFrame(function() {
+        moveTrig(modal, div);
+      });
     }
   };
 
-  var moveTrig = function(trig, modal, div) {
-    var trigProps = div.getBoundingClientRect();
-    var m = modal;
-    var mProps = m.querySelector('#modal-content').getBoundingClientRect();
+  const moveTrig = function(modal, div) {
+    const mProps = modal.querySelector('.modal__info').getBoundingClientRect();
 
     div.style.top = mProps.top + 'px';
     div.style.left = mProps.left + 'px';
 
     div.style.height = mProps.height + 'px';
     div.style.width = mProps.width + 'px';
+
     div.style.backgroundColor = 'white';
     div.style.opacity = 1;
 
     setTimeout(() => {
       window.requestAnimationFrame(function() {
-        open(m, div);
+        open(modal, div);
       });
-    }, 200);
+    }, contentDelay);
   };
 
-  var open = function(m, div) {
+  const open = function(m, div) {
     if (!isOpen) {
-      var info = m.querySelector('.modal__info');
-      var control = m.querySelector('.modal__control');
+      const info = m.querySelector('.modal__info');
+      const control = m.querySelector('.modal__control');
 
       div.style.opacity = 0;
       info.style.opacity = 1;
@@ -79,41 +73,40 @@ var Modal = (function() {
     }
   };
 
-  var close = function(event) {
+  const close = function(event) {
     event.preventDefault();
     event.stopImmediatePropagation();
-    var target = event.target;
-    var modal = document.getElementById('modal');
-    var div = document.getElementById('modal__temp');
-    var info = modal.querySelector('.modal__info');
-    var control = modal.querySelector('.modal__control');
+
+    const target = event.target;
+    const modal = document.getElementById('modal');
+    const div = document.getElementById('modal__temp');
+    const info = modal.querySelector('.modal__info');
+    const control = modal.querySelector('.modal__control');
 
     if (isOpen || target.classList.contains('modal__close')) {
-      
       div.removeAttribute('style');
 
       info.style.opacity = 0;
       control.style.opacity = 0;
 
-      modal.className = 'modal modal__bg';
+      modal.classList.remove('active');
       isOpen = false;
 
-      
       window.requestAnimationFrame(function() {
         div.remove();
       });
     }
   };
 
-  var bindActions = function() {
-    for (var i = 0; i < len; i++) {
-      trigger[i].addEventListener('click', getId, false);
+  const bindActions = function() {
+    for (let i = 0; i < len; i++) {
+      trigger[i].addEventListener('click', handleClick, false);
       closers[0].addEventListener('click', close, false);
       modalsbg[0].addEventListener('click', close, false);
     }
   };
 
-  var init = function() {
+  const init = function() {
     bindActions();
   };
 
